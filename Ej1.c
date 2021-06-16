1a350a21aff4ae10050c17764cde8994ab2b2a0d
facundoaguel <52424329+facundoaguel@users.noreply.github.com>
2021-06-16 17:54:08 -0300

    Add files via upload

---
+++ Ej1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+main(){
+    float Num1 = 109.87;
+    int Num2 = 5;
+    float Num3 = 45.90;
+    printf("Ejercico 1 \n");
+    printf("%1.02f \t  %i  \t  %1.02f \n",Num1,Num2,Num3); // Mostrar numeros
+}   
